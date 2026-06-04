a0b962bd2882e7ce0debfdcb68eff6a7349967c6
Blake Netzeband <blake.netzeband@gmail.com>
2026-06-03 22:19:18 -0600

    add build step for ui lib

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,18 +2,8 @@ import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/c
 import { theme } from '@scandalorian/theme'
 import '@mantine/core/styles.css';
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,7 +18,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className="h-full antialiased"
       {...mantineHtmlProps}
     >
       <head>
